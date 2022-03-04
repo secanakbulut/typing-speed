52137d7c2580b76d546c9572329a9cc149d9cb9a
Secan A. <dariusthegiant@gmail.com>
2022-03-04 20:47:11 +0100

    first cut, render passage and capture input

---
+++ passages.js
@@ -0,0 +1,16 @@
+// short paragraphs to type. kept generic so it works for everyone.
+var PASSAGES = [
+  "the quick brown fox jumps over the lazy dog. it is a classic typing sentence because it uses every letter of the alphabet at least once.",
+  "writing code is mostly about reading code. you spend more time figuring out what already exists than typing new lines into a blank file.",
+  "a small project finished beats a big project planned. ship the rough version, then sand down the parts that bother you the most.",
+  "the river was calm in the early morning. mist sat low over the water and the only sound was a single bird somewhere on the far bank.",
+  "good notes are short. if you write more than a paragraph you will never read it again. one line, the date, and a tag is usually enough.",
+  "she opened the old book and a small piece of paper fell out. it had a phone number on it but no name. the paper smelled of dust and tea.",
+  "computers are very fast at doing exactly what you tell them. that is also the main reason they are so frustrating to work with sometimes.",
+  "the train was late again. nobody on the platform looked surprised. a man with a long coat finished his coffee and folded the empty cup in half.",
+  "learn one keyboard shortcut a week and after a year you will move through your editor without thinking. the trick is to actually use them.",
+  "rain started before he reached the door. by the time he found the key in his pocket his shirt was already stuck to his back and he laughed.",
+  "a function should do one thing. if you find yourself writing the word and in its name, it is probably two functions wearing one hat.",
+  "the cat sat on the warm windowsill and watched the street. cars passed, people walked by, and none of it concerned her in the slightest.",
+  "test your code on the worst input you can think of, then try one worse than that. users will always find the case you did not consider."
+];
